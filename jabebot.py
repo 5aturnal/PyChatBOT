d69a9780abb6cea8faf84d868aaaf0e9b24608f8
solano97 <fsolano@ufl.edu>
2022-04-24 17:49:01 -0400

    Update jabebot.py

--- jabebot.py
+++ jabebot.py
@@ -6,7 +6,7 @@ import openai
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "your-openai-api-key-here"
+openai.api_key = "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"
 completion = openai.Completion()
 
 start_sequence = "\nFeli:"
